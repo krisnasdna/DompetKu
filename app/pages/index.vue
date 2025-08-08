f881b32c355e27f3b557d6d89c66fda081adc452
krisnasdna <krisnasedana2403@gmail.com>
2025-08-08 22:43:17 +0800

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="text-black text-2xl">
+      Hello World
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "shadcn-nuxt",
+    "@vueuse/nuxt",
+    [
+      "@vee-validate/nuxt",
+      {
+        autoImports: true,
+        componentNames: {
+          Form: "VeeForm",
+          Field: "VeeField",
+          FieldArray: "VeeFieldArray",
+          ErrorMessage: "VeeErrorMessage",
+        },
+      },
+    ],
+    "@nuxt/eslint",
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./app/components/ui",
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
